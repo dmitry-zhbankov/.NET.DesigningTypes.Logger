e8a997ee4f5cf407d6210b1e308893f16bb7382f
Dmitry Zhbankov <dmitry.zhbankov@gmail.com>
2019-12-02 17:02:20 +0300

    Add disposing

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -10,34 +10,47 @@ namespace Logger
 
             Console.WriteLine("Default logging");
             logger = new Logger();
-            logger.Error("error message");
-            logger.Error(new Exception("exception message"));
-            logger.Warning("warning message");
-            logger.Info("info message");
+            using ((IDisposable)logger)
+            {
+                logger.Error("error message");
+                logger.Error(new Exception("exception message"));
+                logger.Warning("warning message");
+                logger.Info("info message");
+            }
             Console.WriteLine("");
 
             Console.WriteLine("Console logging");
             logger = new ConsoleLogger();
-            logger.Error("error message");
-            logger.Error(new Exception("exception message"));
-            logger.Warning("warning message");
-            logger.Info("info message");
+            using ((IDisposable)logger)
+            {
+                logger.Error("error message");
+                logger.Error(new Exception("exception message"));
+                logger.Warning("warning message");
+                logger.Info("info message");
+            }
             Console.WriteLine("");
 
             Console.WriteLine("File logging");
             logger = FileLogger.GetInstance();
-            logger.Error("error message");
-            logger.Error(new Exception("exception message"));
-            logger.Warning("warning message");
-            logger.Info("info message");
+            using ((IDisposable)logger)
+            {
+                logger.Error("error message");
+                logger.Error(new Exception("exception message"));
+                logger.Warning("warning message");
+                logger.Info("info message");
+            }
             Console.WriteLine("");
 
             Console.WriteLine("Db logging");
             logger = DbLogger.GetInstance();
-            logger.Error("error message");
-            logger.Error(new Exception("exception message"));
-            logger.Warning("warning message");
-            logger.Info("info message");
+
+            using ((IDisposable)logger)
+            {
+                logger.Error("error message");
+                logger.Error(new Exception("exception message"));
+                logger.Warning("warning message");
+                logger.Info("info message");
+            }
             Console.WriteLine("");
 
             Console.ReadKey();
